6630a95aaae308d607536026acf0e61cd0c10795
William Li <email@williamli.dev>
2023-11-03 22:50:32 +1100

    test logger

--- api/index.py
+++ api/index.py
@@ -1,8 +1,17 @@
+import logging
+
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        
+
+        logging.debug('This is a debug message')
+        logging.info('This is an info message')
+        logging.warning('This is a warning message')
+        logging.error('This is an error message')
+        logging.critical('This is a critical message')
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
